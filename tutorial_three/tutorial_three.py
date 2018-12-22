8a33f31dc8219ff806649ff6bb2b3a26ef1474ba
Victor <victor.e+github@posteo.net>
2018-12-22 20:05:18 +0100

    Sinnvoller Anfang tutorial_three

--- tutorial_three/tutorial_three.py
+++ tutorial_three/tutorial_three.py
@@ -3,10 +3,8 @@
 ##################################################
 # GNU Radio Python Flow Graph
 # Title: Tutorial Three
-# Generated: Thu Dec 20 16:50:54 2018
+# Generated: Sat Dec 22 20:01:07 2018
 ##################################################
-
-from distutils.version import StrictVersion
 
 if __name__ == '__main__':
     import ctypes
@@ -18,14 +16,16 @@ if __name__ == '__main__':
         except:
             print "Warning: failed to XInitThreads()"
 
-from PyQt5 import Qt, QtCore
+from PyQt4 import Qt
 from gnuradio import analog
-from gnuradio import audio
+from gnuradio import blocks
 from gnuradio import eng_notation
 from gnuradio import gr
+from gnuradio import qtgui
 from gnuradio.eng_option import eng_option
 from gnuradio.filter import firdes
 from optparse import OptionParser
+import sip
 import sys
 from gnuradio import qtgui
 
@@ -54,29 +54,94 @@ class tutorial_three(gr.top_block, Qt.QWidget):
         self.top_layout.addLayout(self.top_grid_layout)
 
         self.settings = Qt.QSettings("GNU Radio", "tutorial_three")
+        self.restoreGeometry(self.settings.value("geometry").toByteArray())
 
-        if StrictVersion(Qt.qVersion()) < StrictVersion("5.0.0"):
-            self.restoreGeometry(self.settings.value("geometry").toByteArray())
-        else:
-            self.restoreGeometry(self.settings.value("geometry", type=QtCore.QByteArray))
 
         ##################################################
         # Variables
         ##################################################
         self.samp_rate = samp_rate = 32000
+        self.f_c = f_c = 5500
+        self.ampl = ampl = 0
 
         ##################################################
         # Blocks
         ##################################################
-        self.audio_sink_0 = audio.sink(samp_rate, '', False)
-        self.analog_sig_source_x_1 = analog.sig_source_f(samp_rate, analog.GR_COS_WAVE, 5.5e3, 1, 1)
-        self.analog_sig_source_x_0 = analog.sig_source_f(samp_rate, analog.GR_COS_WAVE, 5e3, 1, 0)
+        self._f_c_tool_bar = Qt.QToolBar(self)
+        self._f_c_tool_bar.addWidget(Qt.QLabel("f_c"+": "))
+        self._f_c_line_edit = Qt.QLineEdit(str(self.f_c))
+        self._f_c_tool_bar.addWidget(self._f_c_line_edit)
+        self._f_c_line_edit.returnPressed.connect(
+        	lambda: self.set_f_c(eng_notation.str_to_num(str(self._f_c_line_edit.text().toAscii()))))
+        self.top_grid_layout.addWidget(self._f_c_tool_bar)
+        self._ampl_tool_bar = Qt.QToolBar(self)
+        self._ampl_tool_bar.addWidget(Qt.QLabel("ampl"+": "))
+        self._ampl_line_edit = Qt.QLineEdit(str(self.ampl))
+        self._ampl_tool_bar.addWidget(self._ampl_line_edit)
+        self._ampl_line_edit.returnPressed.connect(
+        	lambda: self.set_ampl(eng_notation.str_to_num(str(self._ampl_line_edit.text().toAscii()))))
+        self.top_grid_layout.addWidget(self._ampl_tool_bar)
+        self.qtgui_time_sink_x_0 = qtgui.time_sink_f(
+        	1024, #size
+        	samp_rate, #samp_rate
+        	"", #name
+        	1 #number of inputs
+        )
+        self.qtgui_time_sink_x_0.set_update_time(0.10)
+        self.qtgui_time_sink_x_0.set_y_axis(-1, 1)
+
+        self.qtgui_time_sink_x_0.set_y_label('Amplitude', "")
+
+        self.qtgui_time_sink_x_0.enable_tags(-1, True)
+        self.qtgui_time_sink_x_0.set_trigger_mode(qtgui.TRIG_MODE_FREE, qtgui.TRIG_SLOPE_POS, 0.0, 0, 0, "")
+        self.qtgui_time_sink_x_0.enable_autoscale(False)
+        self.qtgui_time_sink_x_0.enable_grid(False)
+        self.qtgui_time_sink_x_0.enable_axis_labels(True)
+        self.qtgui_time_sink_x_0.enable_control_panel(False)
+        self.qtgui_time_sink_x_0.enable_stem_plot(False)
+
+        if not True:
+          self.qtgui_time_sink_x_0.disable_legend()
+
+        labels = ['', '', '', '', '',
+                  '', '', '', '', '']
+        widths = [1, 1, 1, 1, 1,
+                  1, 1, 1, 1, 1]
+        colors = ["blue", "red", "green", "black", "cyan",
+                  "magenta", "yellow", "dark red", "dark green", "blue"]
+        styles = [1, 1, 1, 1, 1,
+                  1, 1, 1, 1, 1]
+        markers = [-1, -1, -1, -1, -1,
+                   -1, -1, -1, -1, -1]
+        alphas = [1.0, 1.0, 1.0, 1.0, 1.0,
+                  1.0, 1.0, 1.0, 1.0, 1.0]
+
+        for i in xrange(1):
+            if len(labels[i]) == 0:
+                self.qtgui_time_sink_x_0.set_line_label(i, "Data {0}".format(i))
+            else:
+                self.qtgui_time_sink_x_0.set_line_label(i, labels[i])
+            self.qtgui_time_sink_x_0.set_line_width(i, widths[i])
+            self.qtgui_time_sink_x_0.set_line_color(i, colors[i])
+            self.qtgui_time_sink_x_0.set_line_style(i, styles[i])
+            self.qtgui_time_sink_x_0.set_line_marker(i, markers[i])
+            self.qtgui_time_sink_x_0.set_line_alpha(i, alphas[i])
+
+        self._qtgui_time_sink_x_0_win = sip.wrapinstance(self.qtgui_time_sink_x_0.pyqwidget(), Qt.QWidget)
+        self.top_grid_layout.addWidget(self._qtgui_time_sink_x_0_win)
+        self.blocks_throttle_0 = blocks.throttle(gr.sizeof_float*1, samp_rate,True)
+        self.blocks_probe_signal_x_0 = blocks.probe_signal_f()
+        self.analog_sig_source_x_1 = analog.sig_source_f(samp_rate, analog.GR_SIN_WAVE, f_c, ampl, 0)
+        self.analog_sig_source_x_0 = analog.sig_source_f(samp_rate, analog.GR_SQR_WAVE, 100e-3, 1, 0)
+
+
 
         ##################################################
         # Connections
         ##################################################
-        self.connect((self.analog_sig_source_x_0, 0), (self.audio_sink_0, 0))
-        self.connect((self.analog_sig_source_x_1, 0), (self.audio_sink_0, 1))
+        self.connect((self.analog_sig_source_x_0, 0), (self.blocks_throttle_0, 0))
+        self.connect((self.analog_sig_source_x_1, 0), (self.qtgui_time_sink_x_0, 0))
+        self.connect((self.blocks_throttle_0, 0), (self.blocks_probe_signal_x_0, 0))
 
     def closeEvent(self, event):
         self.settings = Qt.QSettings("GNU Radio", "tutorial_three")
@@ -88,13 +153,32 @@ class tutorial_three(gr.top_block, Qt.QWidget):
 
     def set_samp_rate(self, samp_rate):
         self.samp_rate = samp_rate
+        self.qtgui_time_sink_x_0.set_samp_rate(self.samp_rate)
+        self.blocks_throttle_0.set_sample_rate(self.samp_rate)
         self.analog_sig_source_x_1.set_sampling_freq(self.samp_rate)
         self.analog_sig_source_x_0.set_sampling_freq(self.samp_rate)
+
+    def get_f_c(self):
+        return self.f_c
+
+    def set_f_c(self, f_c):
+        self.f_c = f_c
+        Qt.QMetaObject.invokeMethod(self._f_c_line_edit, "setText", Qt.Q_ARG("QString", eng_notation.num_to_str(self.f_c)))
+        self.analog_sig_source_x_1.set_frequency(self.f_c)
+
+    def get_ampl(self):
+        return self.ampl
+
+    def set_ampl(self, ampl):
+        self.ampl = ampl
+        Qt.QMetaObject.invokeMethod(self._ampl_line_edit, "setText", Qt.Q_ARG("QString", eng_notation.num_to_str(self.ampl)))
+        self.analog_sig_source_x_1.set_amplitude(self.ampl)
 
 
 def main(top_block_cls=tutorial_three, options=None):
 
-    if StrictVersion("4.5.0") <= StrictVersion(Qt.qVersion()) < StrictVersion("5.0.0"):
+    from distutils.version import StrictVersion
+    if StrictVersion(Qt.qVersion()) >= StrictVersion("4.5.0"):
         style = gr.prefs().get_string('qtgui', 'style', 'raster')
         Qt.QApplication.setGraphicsSystem(style)
     qapp = Qt.QApplication(sys.argv)
@@ -106,7 +190,7 @@ def main(top_block_cls=tutorial_three, options=None):
     def quitting():
         tb.stop()
         tb.wait()
-    qapp.aboutToQuit.connect(quitting)
+    qapp.connect(qapp, Qt.SIGNAL("aboutToQuit()"), quitting)
     qapp.exec_()
 
 
